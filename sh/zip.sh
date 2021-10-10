68771abb73fd9f6a4cd8cd2e8f51c4838d0418f8
Ryan <ryanpivovar@gmail.com>
2021-10-10 12:41:02 -0400

    update zip command to rename last-published.js to index.js

--- sh/zip.sh
+++ sh/zip.sh
@@ -1,4 +1,5 @@
 npm run clean
 npm run build
-zip -j dist/badges-last-published.zip dist/last-published.js
-rm dist/last-published.js
+mv dist/last-published.js dist/index.js
+zip -j dist/badges-last-published.zip dist/index.js
+rm dist/*.js
