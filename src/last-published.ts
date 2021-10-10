1a8bc34ff896c6c473b8aa2e33005da15d77d636
Ryan <ryanpivovar@gmail.com>
2021-10-10 11:20:33 -0400

    sync src with boilerplate lambda code

--- src/last-published.ts
+++ src/last-published.ts
@@ -1,1 +1,7 @@
-module.exports = "hello";
+exports.handler = async (event: unknown) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
