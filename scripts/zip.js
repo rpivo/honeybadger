25a68baca999d7864ea7dc8bc0b7a464dd1948ad
Ryan <ryanpivovar@gmail.com>
2021-10-10 23:40:33 -0400

    remove js suffix from zip names

--- scripts/zip.js
+++ scripts/zip.js
@@ -5,6 +5,6 @@ const jsFiles = readdirSync("./dist").filter((file) => file.endsWith(".js"));
 
 for (const file of jsFiles) {
   execSync(`mv dist/${file} dist/index.js`);
-  execSync(`zip -j dist/badges-${file}.zip dist/index.js`);
+  execSync(`zip -j dist/badges-${file.split(".")[0]}.zip dist/index.js`);
   execSync(`rm dist/index.js`);
 }
