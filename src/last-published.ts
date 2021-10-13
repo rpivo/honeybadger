93362f79a1c64612302ff0c6de26c03c63fa3379
Ryan <ryanpivovar@gmail.com>
2021-10-13 18:49:54 -0400

    add last-published lambda

--- src/last-published.ts
+++ src/last-published.ts
@@ -7,13 +7,18 @@ interface APIGatewayEvent {
   };
 }
 
+enum Colors {
+  GREEN = "2AB358",
+  YELLOW = "F0BB07",
+  RED = "EB1300",
+}
+
 exports.handler = async (event: APIGatewayEvent) => {
   const { package: packageName } = event.pathParameters;
 
   return await new Promise((resolve, reject) => {
     const format = <Format>{
-      color: "2AB358",
-      label: "Latest Version",
+      label: "Last Published",
       labelColor: "253B4D",
       message: "Bad Fetch",
       style: 'plastic'
@@ -33,7 +38,56 @@ exports.handler = async (event: APIGatewayEvent) => {
 
         response.on("end", () => {
           const json = JSON.parse(data);
-          format.message = json["dist-tags"].latest;
+          const latestVersion = json["dist-tags"].latest;
+
+          const publishDate = new Date(json.time[latestVersion]);
+          const currentDate = new Date();
+
+          const yearPublished = publishDate.getFullYear();
+          const currentYear = currentDate.getFullYear();
+
+          if (yearPublished === currentYear) {
+            const monthPublished = publishDate.getMonth();
+            const currentMonth = currentDate.getMonth();
+  
+            if (monthPublished === currentMonth) {
+              format.color = Colors.GREEN;
+  
+              const dayPublished = publishDate.getDate();
+              const currentDay = currentDate.getDate();
+  
+              if (dayPublished === currentDay) {
+                format.message = "Today";
+              } else {
+                const dayDifference = currentDay - dayPublished;
+                format.message = `${dayDifference} day${dayDifference === 1 ? '' : 's'} ago`;
+              }
+            } else {
+              const monthDifference = currentMonth - monthPublished;
+              format.color = monthDifference >= 6 ? Colors.YELLOW : Colors.GREEN;
+              format.message = `${monthDifference} month${monthDifference === 1 ? '' : 's'} ago`;
+            }
+          } else {
+            const yearDifference = currentYear - yearPublished;
+
+            if (yearDifference === 1) {
+              const monthPublished = publishDate.getMonth();
+              const currentMonth = currentDate.getMonth();
+              
+              if (monthPublished > currentMonth) {
+                const monthDifference = (currentMonth - monthPublished) + 12;
+                format.color = monthDifference >= 6 ? Colors.YELLOW : Colors.GREEN;
+                format.message = `${monthDifference === 1 ? '' : 's'} months ago`;
+              } else {
+                const monthDifference = currentMonth - monthPublished;
+                format.color = monthDifference >= 6 ? Colors.YELLOW : Colors.GREEN;
+                format.message = `${monthDifference === 1 ? '' : 's'} months ago`;
+             }
+            } else {
+              format.color = Colors.RED;
+              format.message = `${yearDifference} years ago`;
+            }
+          }
 
           resolve({
             statusCode: 200,
